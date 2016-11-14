6b1b44fc539de94e4fd41fc14e9dbc430a64a207
hpw123 <424346976@qq.com>
2016-11-14 14:50:15 +0800

    init

--- rrcp_web/src/main/java/com/rrcp/web/controller/UserController.java
+++ rrcp_web/src/main/java/com/rrcp/web/controller/UserController.java
@@ -23,17 +23,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-	/*@RequestMapping(value = "/list", method = RequestMethod.GET)
-    public String list(Model entity, Integer offset, Integer limit) {
-		LOG.info("invoke----------/user/list");
-		offset = offset == null ? 0 : offset;//默认便宜0
-		limit = limit == null ? 50 : limit;//默认展示50条
-		List<User> list = userService.getUserList(offset, limit);
-		entity.addAttribute("userlist", list);
-		return "userlist";
-	}*/
-
-
     /**
      * 摒弃jsp页面通过ajax接口做到真正意义上的前后分离
      *
